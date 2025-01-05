d46fb4abb3de98d4769f0ba2f82d3d4d3542673b
Pit1024 <62759808+Pit1024@users.noreply.github.com>
2025-01-06 01:07:35 +0300

    Update main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net"
 	"net/http"
 )
 
@@ -14,43 +15,94 @@ func main() {
 
 // функция run будет полезна при инициализации зависимостей сервера перед запуском
 func run() error {
-
-	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, POSThook)
-	mux.HandleFunc(`/EwHXdJfB`, GEThook)
-	return http.ListenAndServe(`:8080`, mux)
+	return http.ListenAndServe(`:8080`, http.HandlerFunc(webhook))
 }
 
-var URLtoken map[string]string = map[string]string{
+var URLmap map[string][]string
 
-	"shortener": "http://localhost:8080/EwHXdJfB",
-	"full":      "http://yandex.ru/",
-}
+// функция webhook — обработчик HTTP-запроса
+func webhook(w http.ResponseWriter, r *http.Request) {
 
-func POSThook(w http.ResponseWriter, r *http.Request) {
+	// парсим переданный шаблон, проверяем на ошибки
+	host, port, err := net.SplitHostPort(r.Host)
+	if err != nil {
 
-	if r.Method != http.MethodPost { //|| r.Header.Get("Content-Type") != "text/plain" {
-		// проверка на POST-запросы и на заголовок
 		http.Error(w, "", 400)
-		return
 	}
 
-	w.Header().Set("Content-Type", "text/plain")
-	w.WriteHeader(201)
-	w.Write([]byte(URLtoken["shortener"]))
+	// сохраняем пришедший адрес
+	URLmap = map[string][]string{"shortener": {host, port}}
 
-}
+	// Если запрос POST и нет /{id}, то выдаем хост и порт и статус 201
+	if r.Method == http.MethodPost && port == "" {
+		http.Redirect(w, r, host, 201)
 
-func GEThook(w http.ResponseWriter, r *http.Request) {
+		// Если запрос GET и есть /{id}, то выдаем обычный хост и статус 201
+	} else if r.Method == http.MethodGet && port != "" {
+		http.Redirect(w, r, host, 307)
 
-	if r.Method != http.MethodGet {
-		// проверка на GET-запросы
+		// в любом другом случае ошибка 400
+	} else {
+
 		http.Error(w, "", 400)
-		return
 	}
 
-	w.Header().Set("Location", URLtoken["full"])
-	w.WriteHeader(307)
-	w.Write([]byte(URLtoken["full"]))
-
 }
+
+//OLD
+// package main
+
+// import (
+// 	"net/http"
+// )
+
+// // функция main вызывается автоматически при запуске приложения
+// func main() {
+// 	if err := run(); err != nil {
+// 		panic(err)
+// 	}
+
+// }
+
+// // функция run будет полезна при инициализации зависимостей сервера перед запуском
+// func run() error {
+
+// 	mux := http.NewServeMux()
+// 	mux.HandleFunc(`/`, POSThook)
+// 	mux.HandleFunc(`/EwHXdJfB`, GEThook)
+// 	return http.ListenAndServe(`:8080`, mux)
+// }
+
+// var URLtoken map[string]string = map[string]string{
+
+// 	"shortener": "http://localhost:8080/EwHXdJfB",
+// 	"full":      "http://yandex.ru/",
+// }
+
+// func POSThook(w http.ResponseWriter, r *http.Request) {
+
+// 	if r.Method != http.MethodPost { //|| r.Header.Get("Content-Type") != "text/plain" {
+// 		// проверка на POST-запросы и на заголовок
+// 		http.Error(w, "", 400)
+// 		return
+// 	}
+
+// 	w.Header().Set("Content-Type", "text/plain")
+// 	w.WriteHeader(201)
+// 	w.Write([]byte(URLtoken["shortener"]))
+
+// }
+
+// func GEThook(w http.ResponseWriter, r *http.Request) {
+
+// 	if r.Method != http.MethodGet {
+// 		// проверка на GET-запросы
+// 		http.Error(w, "", 400)
+// 		return
+// 	}
+
+// 	w.Header().Set("Location", URLtoken["full"])
+// 	w.WriteHeader(307)
+// 	w.Write([]byte(URLtoken["full"]))
+
+// }
